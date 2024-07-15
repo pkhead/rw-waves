472a912c4b4f307dc9bfee6fa4305064d3b37342
pkhead <chromosoze@gmail.com>
2024-07-14 21:39:04 -0500

    working player lives system

--- plugin/WavesGameSession.cs
+++ plugin/WavesGameSession.cs
@@ -4,6 +4,7 @@ using ArenaBehaviors;
 using UnityEngine;
 using Mono.Cecil.Cil;
 using MonoMod.Cil;
+using RWCustom;
 namespace WavesMod;
 using Random = UnityEngine.Random;
 
@@ -11,6 +12,7 @@ class WavesGameSession : ArenaGameSession
 {
     // if all tracked creatures are dead, initiate the next wave
     private readonly List<AbstractCreature> trackedCreatures;
+    private readonly HashSet<AbstractCreature> permaDeadPlayers;
     private WavesCreatureSpawner creatureSpawner = null;
 
     public int wave = -1;
@@ -20,6 +22,7 @@ class WavesGameSession : ArenaGameSession
     {
         rainCycleTimeInMinutes = 0;
         trackedCreatures = new();
+        permaDeadPlayers = new();
         
         arenaSitting.gameTypeSetup.levelItems = true;
         arenaSitting.gameTypeSetup.spearHitScore = 0;
@@ -67,11 +70,122 @@ class WavesGameSession : ArenaGameSession
         game.cameras[0].hud?.textPrompt?.AddMessage("Wave " + (wave+1), 0, 240, false, false);
     }
 
+    private void SpawnPlayer(int playerIndex)
+    {
+        var playerList = arenaSitting.players;
+
+        int exits = this.game.world.GetAbstractRoom(0).exits;
+        int[] array = new int[exits];
+        
+        int num2 = UnityEngine.Random.Range(0, exits);
+		float num3 = float.MinValue;
+		for (int m = 0; m < exits; m++)
+		{
+			float num4 = UnityEngine.Random.value - (float)array[m] * 1000f;
+			IntVector2 startTile = room.ShortcutLeadingToNode(m).StartTile;
+			for (int n = 0; n < exits; n++)
+			{
+				if (n != m && array[n] > 0)
+				{
+					num4 += Mathf.Clamp(startTile.FloatDist(room.ShortcutLeadingToNode(n).StartTile), 8f, 17f) * UnityEngine.Random.value;
+				}
+			}
+			if (num4 > num3)
+			{
+				num2 = m;
+				num3 = num4;
+			}
+		}
+		array[num2]++;
+		AbstractCreature abstractCreature = new AbstractCreature(
+            world: this.game.world,
+            creatureTemplate: StaticWorld.GetCreatureTemplate("Slugcat"),
+            realizedCreature: null,
+            pos: new WorldCoordinate(0, -1, -1, -1),
+            ID: new EntityID(-1, playerList[playerIndex].playerNumber)
+        );
+		if (ModManager.MSC && playerIndex == 0)
+		{
+			this.game.cameras[0].followAbstractCreature = abstractCreature;
+		}
+		if (this.chMeta != null)
+		{
+			abstractCreature.state = new PlayerState(abstractCreature, playerList[playerIndex].playerNumber, this.characterStats_Mplayer[0].name, false);
+		}
+		else
+		{
+			abstractCreature.state = new PlayerState(abstractCreature, playerList[playerIndex].playerNumber, new SlugcatStats.Name(ExtEnum<SlugcatStats.Name>.values.GetEntry(playerList[playerIndex].playerNumber), false), false);
+		}
+		abstractCreature.Realize();
+		ShortcutHandler.ShortCutVessel shortCutVessel = new ShortcutHandler.ShortCutVessel(new IntVector2(-1, -1), abstractCreature.realizedCreature, this.game.world.GetAbstractRoom(0), 0);
+		shortCutVessel.entranceNode = num2;
+		shortCutVessel.room = this.game.world.GetAbstractRoom(0);
+		abstractCreature.pos.room = this.game.world.offScreenDen.index;
+		this.game.shortcuts.betweenRoomsWaitingLobby.Add(shortCutVessel);
+
+        // replace old player AbstractCreature in the Players list with the new one
+        bool wasPlayerAdded = false;
+        for (int i = 0; i < Players.Count; i++)
+        {
+            if (Players[i].state is PlayerState state && state.playerNumber == playerList[playerIndex].playerNumber)
+            {
+                wasPlayerAdded = true;
+                Players[i] = abstractCreature;
+                break;
+            }
+        }
+
+        if (!wasPlayerAdded) AddPlayer(abstractCreature);
+    }
+
+    private void PlayerPermaDie(AbstractCreature player)
+    {
+        if (!Players.Contains(player)) return;
+        Debug.Log("player permadie");
+        permaDeadPlayers.Add(player);
+    }
+
     private void NewWave()
     {
         wave++;
 
         Debug.Log("Spawn wave " + wave);
+
+        // respawn previously dead players
+        // if player count is 0, players might not have spawned yet
+        if (Players.Count > 0)
+        {
+            for (int i = 0; i < arenaSitting.players.Count; i++)
+            {
+                var playerNumber = arenaSitting.players[i].playerNumber;
+
+                // check that player isn't dead
+                var playerCreature = Players.Find(p => (p.state as PlayerState).playerNumber == playerNumber)
+                    ?? throw new NullReferenceException($"could not find creature for player with number {playerNumber}");
+                if (playerCreature.state.alive) continue;
+                
+                // respawn the player if they have enough lives
+                if (ArenaSittingHooks.TryGetData(arenaSitting, out var data))
+                {
+                    // if on normal mode and player isn't perma-dead,
+                    // they get an extra chance
+                    if (data.difficulty == WavesDifficultyOption.Normal && !permaDeadPlayers.Contains(playerCreature))
+                        data.playerLives[playerNumber]++;
+                    
+                    if (data.playerLives[playerNumber] >= 1)
+                    {
+                        data.playerLives[playerNumber]--;
+
+                        // do dissolve animation for old player corpse
+                        if (playerCreature.realizedCreature?.room is not null)
+                            playerCreature.realizedCreature.room.AddObject(new DespawnAnimation(playerCreature.realizedCreature));
+                        
+                        SpawnPlayer(i);
+                    }
+                }
+            }
+            permaDeadPlayers.Clear();
+        }
 
         var abstractRoom = game.world.GetAbstractRoom(0);
 
@@ -317,6 +431,72 @@ class WavesGameSession : ArenaGameSession
             self.Destroy();
         };
 
+        // perma-die player when they are destroyed
+        // this occurs when player falls off room and when player is eaten by rot or worm grass
+        // does not occur when a player is dragged into a den, sky or ground 
+        On.Player.Destroy += (On.Player.orig_Destroy orig, Player self) =>
+        {
+            orig(self);
+
+            if (self.room.game.session is WavesGameSession wavesSesh)
+            {
+                wavesSesh.PlayerPermaDie(self.abstractCreature);
+            }
+        };
+
+        On.Vulture.AccessSkyGate += (
+            On.Vulture.orig_AccessSkyGate orig, Vulture self,
+            WorldCoordinate start, WorldCoordinate dest
+        ) =>
+        {
+            orig(self, start, dest);
+            if (self.room.game.session is WavesGameSession wavesSesh)
+            {
+                foreach (var grasp in self.grasps)
+                {
+                    if (grasp?.grabbed is Player player)
+                    {
+                        wavesSesh.PlayerPermaDie(player.abstractCreature);
+                    }
+                }
+            }
+        };
+
+        On.AbstractCreature.IsEnteringDen += (
+            On.AbstractCreature.orig_IsEnteringDen orig, AbstractCreature self,
+            WorldCoordinate den
+        ) =>
+        {
+            if (self.world.game.session is WavesGameSession wavesSesh)
+            {
+                if (self.creatureTemplate.quantified && self.creatureTemplate.type == CreatureTemplate.Type.Fly)
+                {
+                    orig(self, den);
+                    return;
+                }
+
+                for (int i = self.stuckObjects.Count - 1; i >= 0; i--)
+                {
+                    if (i < self.stuckObjects.Count && self.stuckObjects[i] is AbstractPhysicalObject.CreatureGripStick && self.stuckObjects[i].A == self)
+                    {
+                        if (self.stuckObjects[i].B is AbstractCreature)
+                        {
+                            if ((self.stuckObjects[i].B as AbstractCreature).creatureTemplate.type == CreatureTemplate.Type.Slugcat)
+                            {
+                                wavesSesh.PlayerPermaDie(self.stuckObjects[i].B as AbstractCreature);
+                            }
+                        }
+                    }
+                }
+
+                orig(self, den);
+                return;
+            }
+
+            orig(self, den);
+        };
+
+        // change whether or not a player spawns on a new session
         IL.ArenaGameSession.SpawnPlayers += (il) =>
         {
             try
@@ -358,21 +538,22 @@ class WavesGameSession : ArenaGameSession
                 cursor.EmitDelegate((ArenaGameSession self, int playerIndex, List<ArenaSitting.ArenaPlayer> playerList) =>
                 {
                     if (self.game.session is not WavesGameSession)
-                        return false;
+                        return true;
 
                     // if the given player has at least one life, consume it to spawn the player
                     if (ArenaSittingHooks.TryGetData(self.arenaSitting, out var data))
                     {
                         var playerNumber = playerList[playerIndex].playerNumber;
-                        Debug.Log($"Spawn player {playerNumber}: Has {data.playerLives[playerNumber]} lives");
                         if (data.playerLives[playerNumber] >= 1)
                         {
                             data.playerLives[playerNumber]--;
                             return true;
                         }
+
+                        return false;
                     }
 
-                    return false;
+                    return true;
                 });
                 cursor.Emit(OpCodes.Brfalse, forLoopContinue);
             }
